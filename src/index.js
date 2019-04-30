de5fc8cf852c122f6895417344bf1ae14fb220c1
LisaBurleson <lisa.burleson.01@gmail.com>
2019-04-29 21:39:39 -1000

    added some reference ways to scale out resolvers

--- src/index.js
+++ src/index.js
@@ -14,3 +14,19 @@ const server = new GraphQLServer( {schema});
 server.start( ( server ) => {
   console.log( `Server listening on http://localhost:${server.port}` );
 } );
+
+/*
+import Query from './resolvers/Query'
+import Mutation from './resolvers/Mutation'
+import User from './resolvers/User'
+import Project from './resolvers/Project'
+import Task from './resolvers/Task'
+
+
+const server = new GraphQLServer( {
+  typeDefs: './src/schema.graphql',
+  resolvers: {
+    Task, Project, User
+  }
+} );
+ */
